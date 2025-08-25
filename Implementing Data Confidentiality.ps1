e8489f0b8585d0bca6d5d696ada925f8e7ac5a1c
josephr2623-ux <josephr2623@gmail.com>
2025-08-25 15:39:22 +0800

    Add files via upload

---
+++ Implementing Data Confidentiality.ps1
@@ -0,0 +1,10 @@
+﻿Implementing Data Confidentiality
+using Public Keys:
+@coreBABA:
+config t
+ip domain-name pfda.gov.ph
+crypto key generate rsa
+!! tanong, anong Keysize: 256, 512, 1024
+ip ssh version 2
+ip ssh time-out 120
+end
